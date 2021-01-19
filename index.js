9bf56c653a40a83f1f4f4845e0d05f90c3c0e95f
D0wzy <52222574+D0wzy@users.noreply.github.com>
2021-01-19 23:52:25 +0100

    Init repo

---
+++ index.js
@@ -0,0 +1,56 @@
+//comme ça
+
+//non gros flemme & la team est déjà pleine :/
+
+
+/* Modules */
+const express = require('express')
+const moment = require('moment')
+const fs = require('fs')
+
+const config = require('./config.json')
+const app = express()
+const port = 31337
+
+app.listen(port, async() => {
+    Logger.info(`App listening on port ${port}`)
+})
+
+app.set('view engine', 'ejs');
+app.use(express.static('medias'));
+
+
+app.use((req, res) => {
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+});
+
+app.get('/m/:id', async(req, res) => {
+    console.log(req.params.id)
+
+    if (!fs.existsSync(`./medias/${req.params.id}`)) return res.status(200).send({
+        error: "Unknow id"
+    })
+
+    const { birthtime } = fs.statSync(`./medias/${req.params.id}`);
+
+    const createdAt = moment(Date.parse(birthtime).getTIme()).format("YYYY/MM/DD") + "at" + moment(Date.parse(birthtime).getTIme()).format("HH:mm:ss")
+    res.render('./index.html', {
+        name: req.params.id,
+        link: config + `m/${id}`,
+        createdAt
+    })
+})
+
+
+/*LOGGER*/
+function Logger(type, text) {
+    const time = moment(Date.now()).format("HH:mm:ss")
+    console.log(`[${time}] [${type.toUpperCase()}] ${text}`)
+}
+
+Logger.info = async(text) => {
+    Logger('info', text)
+}
