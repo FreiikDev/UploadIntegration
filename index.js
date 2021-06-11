2ec66fda253846972a68705f02f2db69b3cf6a1b
D0wzy <52222574+D0wzy@users.noreply.github.com>
2021-06-11 16:40:35 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require('express')
 const moment = require("moment-timezone");
 const fs = require('fs')
 
-const config = require('./config.json')
+const config = {
+    app: {
+        host: 'https://s.voltbot.xyz/',
+        port: 80
+    }
+};
+
 const app = express();
 const port = config.app.port
 
@@ -15,25 +21,59 @@ app.set('view engine', 'ejs');;
 app.use("/medias", express.static('medias'));
 
 app.get('/:id', async (req, res) => {
-    if (!fs.existsSync(`./medias/${req.params.id}`)) return res.status(404).send("cc")
+
+    if (!fs.existsSync(`./medias/${req.params.id}`)) return res.status(404).send("Tu t'es perdu ? Rejoins le meilleur serveur français ! discord.gg/chillfr")
 
     const { birthtime, size } = fs.statSync(`./medias/${req.params.id}`);
-    const createdAt = `${moment.tz(birthtime, "Europe/Paris").format("DD/MM/YYYY")} at ${moment.tz(birthtime, "Europe/Paris").format("HH:mm:ss")}`
 
     function getFileSize(size) {
         const i = Math.floor(Math.log(size) / Math.log(1024));
         return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i];
     }
 
-    res.render('./index.ejs', {
-        name: req.params.id,
-        link: config.app.host + `medias/${req.params.id}`,
-        createdAt,
-        size: getFileSize(size),
-        show: async () => {
-            res.redirect(config.app.host + `medias/${req.params.id}`)
-        }
-    })
+    if (req.headers.host.includes("chillfr.com")) {
+        res.render('./index.ejs', {
+            color: "#90ff8a",
+            nameWeb: "ChillFR — Capture",
+            name: req.params.id,
+            link: config.app.host + `medias/${req.params.id}`,
+            created: "Téléversé le " + `${moment.tz(birthtime, "Europe/Paris").format("DD/MM/YYYY")} à ${moment.tz(birthtime, "Europe/Paris").format("HH:mm:ss")}`,
+            size: getFileSize(size),
+            show: async () => {
+                res.redirect(config.app.host + `medias/${req.params.id}`)
+            }
+        })
+        return;
+    } else if (req.headers.host.includes("dowzy.fr")) {
+        res.render('./index.ejs', {
+            color: "#2F3136",
+            nameWeb: "D0wzy — Capture d'écran",
+            name: req.params.id,
+            link: config.app.host + `medias/${req.params.id}`,
+            created: "Téléversé le " + `${moment.tz(birthtime, "Europe/Paris").format("DD/MM/YYYY")} à ${moment.tz(birthtime, "Europe/Paris").format("HH:mm:ss")}`,
+            size: getFileSize(size),
+        })
+        return;
+    } else if (req.headers.host.includes("support-accountweb.fr")) {
+        res.render('./index.ejs', {
+            color: "#ffb330",
+            nameWeb: "PayPal Official — Screenshot",
+            name: req.params.id,
+            link: config.app.host + `medias/${req.params.id}`,
+            created: "Uploaded on " + `${moment.tz(birthtime, "Europe/Paris").format("MM/DD/YYYY")} at ${moment.tz(birthtime, "Europe/Paris").format("HH:mm:ss")}`,
+            size: getFileSize(size),
+        })
+    }
+    else {
+        res.render('./index.ejs', {
+            color: "#ffb330",
+            nameWeb: "VoltBot — Screenshot",
+            name: req.params.id,
+            link: config.app.host + `medias/${req.params.id}`,
+            created: "Uploaded on " + `${moment.tz(birthtime, "Europe/Paris").format("MM/DD/YYYY")} at ${moment.tz(birthtime, "Europe/Paris").format("HH:mm:ss")}`,
+            size: getFileSize(size),
+        })
+    }
 })
 
 
